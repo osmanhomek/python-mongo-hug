209ca758c9ba324372b88bb2fad8bde753bb3e57
Osman Homek <osman@geonni.com>
2016-10-16 23:55:57 +0300

    before koding

--- cards.py
+++ cards.py
@@ -7,22 +7,25 @@ from bson import json_util, ObjectId
 def getCards(id: hug.types.text, pid: hug.types.text):
     #id : section_id
     #pid : user_id
-    limitval = 5
+    limitval = 3
 
     client = MongoClient('mongodb://usr_gunde_5dk:Rrd3b7yk28rbAwZb@ds035036.mlab.com:35036/gunde5dkcom')
     db = client.gunde5dkcom
 
     #viewed cards
     result_viewed_cards = db.lpath.find({"user_id":ObjectId(pid),"section_id":ObjectId(id)}, {"_id":0, "viewed_card_ids":1 }).limit(1)
-    #kayit var-yok kontrolü
+    #kayit var-yok kontrolü eklenecek
     records = result_viewed_cards[0]
-    print(records)
-    #elimizdeki id lerden olmayan 5 kaydı aşağıdan alacağız
+    viewed_card_ids = records["viewed_card_ids"]
+    print(viewed_card_ids)
+    print(viewed_card_ids[0])
 
+    #tum kayitlar
+    result_cards = db.cards.find({"section_id":ObjectId(id)}, {"section_id":0 } )
+    for card in result_cards:
+        print(card)
 
-
-    result_cards = db.cards.find({"section_id":ObjectId(id)}, {"section_id":0 } ).limit(limitval)
-    #"source":1, "target":1, "additional1":1, "additional2":1, "additional3":1, "avatar":1
+    result_cards = db.cards.find({"section_id":ObjectId(id),"viewed_card_ids":ObjectId(viewed_card_ids[0])}, {"section_id":0 } ).limit(limitval)
     list_cards = list(result_cards)
 
     dictCards = {}
--- sections.py
+++ sections.py
@@ -4,11 +4,16 @@ from pymongo import MongoClient
 import json
 from bson import json_util, ObjectId
 
-def getSections(id: hug.types.text):
+def getSections(id: hug.types.uuid):
     client = MongoClient('mongodb://usr_gunde_5dk:Rrd3b7yk28rbAwZb@ds035036.mlab.com:35036/gunde5dkcom')
     db = client.gunde5dkcom
+
     result = db.packages.find_one({"application_id":ObjectId(id)})
+    #select * from packages where application_id = ID
+
     packages_sections = result["sections"]
+
+
     listSections = []
     for section_id in packages_sections:
         dict_sections = db.sections.find({"_id":section_id})
